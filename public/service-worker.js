493b95739a355a9aff37804439f97327fb49f637
Carlos Rojas <carlosrb206@gmail.com>
2022-01-30 18:43:06 -0500

    server

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,62 +1,48 @@
-const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'version_01';
+const APP_PREFIX = 'budget-tracker-';
+const VERSION = 'v1.0';
 const CACHE_NAME = APP_PREFIX + VERSION;
-
 const FILES_TO_CACHE = [
     './index.html',
-    './css/styles.css',
-    './js/index.js',
     './js/idb.js',
-    './manifest.json',
-    './icons/icon-512x512.png',
-    './icons/icon-384x384.png',
-    './icons/icon-192x192.png',
-    './icons/icon-152x152.png',
-    './icons/icon-144x144.png',
-    './icons/icon-128x128.png',
-    './icons/icon-96x96.png',
-    './icons/icon-72x72.png'
+    './js/index.js',
+    './css/styles.css',
+    './manifest.json'
 ];
 
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME);
-            return cache.addAll(FILES_TO_CACHE)
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            return cache.addAll(FILES_TO_CACHE);
         })
     )
 });
 
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeeplist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeeplist.push(CACHE_NAME);
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((resp) => {
+            return resp || fetch(event.request).then((response) => {
+                let responseClone = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseClone);
+                });
 
-            return Promise.all(keyList.map(function (key, i) {
+                return response;
+            }).catch(err => console.log(err));
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    let cacheKeeplist = APP_PREFIX;
+
+    event.waitUntil(
+        caches.keys().then((keylist) => {
+            return Promise.all(keylist.map((key) => {
                 if (cacheKeeplist.indexOf(key) === -1) {
-                    console.log('deleting cache : ' + keyList[i] );
-                    return caches.delete(keyList[i]);
+                    return caches.delete(key);
                 }
             }));
         })
-    )
+    );
 });
-
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function (response) {
-            if (response) { // if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url);
-                return response
-            } else {       // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url);
-                return fetch(e.request)
-            }
-
-        }),
-    )
-})
